b573ee08d294aba2ee53cf9ed74d48b789a9581e
rail <rail.shabayev@mail.ru>
2023-08-15 06:41:00 +0400

    feat: Add main CharacterMakerApplication class
    Add main CharacterMakerApplication class to CharacterMakerService

---
+++ character-maker-cervice/src/main/java/org/rail/project/CharacterMakerApplication.java
@@ -0,0 +1,11 @@
+package org.rail.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterMakerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CharacterMakerApplication.class);
+    }
+}
