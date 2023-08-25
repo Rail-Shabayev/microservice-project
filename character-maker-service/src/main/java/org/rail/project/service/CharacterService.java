abc0383e407d1a93575df0a1e3fffa93027971b8
rail <rail.shabayev@mail.ru>
2023-08-25 06:55:09 +0400

    feat: Make CharacterService call the remote microservice
    Add: CharacterService makes GET call to /api/characters in
    order to fetch existing character's names. It uses
    WebClient.Builder to do that.
    
    Response from /api/characters is converted to
    List<CharacterDto> to pass it to an if-else function.
    The function checks whether CharacterDto object from POST
    request has the same name as names from List<CharacterDto>.
    If request's object has any match with these names it
    throws RuntimeException. Otherwise, the object is saved to
    a repository.
    
    Also add Transactional annotations to class to mark it as
    a transactional.

--- character-maker-service/src/main/java/org/rail/project/service/CharacterService.java
+++ character-maker-service/src/main/java/org/rail/project/service/CharacterService.java
@@ -1,19 +1,45 @@
 package org.rail.project.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.rail.project.dto.CharacterDto;
 import org.rail.project.model.Character;
 import org.rail.project.repository.CharacterRepository;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CharacterService {
 
     private final CharacterRepository characterRepository;
+    private final WebClient.Builder webClientBuilder;
+    ObjectMapper objectMapper = new ObjectMapper();
 
-    public void saveCharacter(CharacterDto characterDto) {
-        characterRepository.save(mapToModel(characterDto));
+    public void saveCharacter(CharacterDto characterDto) throws JsonProcessingException {
+        String existingCharacters = Objects.requireNonNull(webClientBuilder.build()
+                .get()
+                .uri("http://characters-service/api/characters")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block());
+        List<CharacterDto> characterDtos = objectMapper.readValue(existingCharacters, new TypeReference<>(){});
+
+        if (characterDtos.stream()
+                .map(CharacterDto::getName)
+                .anyMatch(s -> s.equals(characterDto.getName())))
+            throw new RuntimeException("A character with that name is already exists");
+        else
+            characterRepository.save(mapToModel(characterDto));
     }
 
     public static Character mapToModel(CharacterDto characterDto) {
