5c8325246c7db79f5ab9f25108bb8b35656bd3af
rail <rail.shabayev@mail.ru>
2023-08-26 08:51:23 +0400

    perf: Permit all requests
    Disable a jwt resource server and permit requests
    to secured endpoins.

--- api-gateway/src/main/java/org/rail/project/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/rail/project/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package org.rail.project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -15,9 +14,19 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
         return httpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
-                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
-                        .jwt(Customizer.withDefaults()))
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().permitAll())
+//
+//                 Commented an authorization functionality to save up RAM resources
+//
+//                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers(
+//                        "/v3/api-docs",
+//                        "/v3/api-docs/**",
+//                        "/swagger-ui.html").permitAll())
+//                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+//                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+//                        .jwt(Customizer.withDefaults()))
                 .build();
     }
+
 }
